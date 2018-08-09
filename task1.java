72c953a7780ff21579a09f267098ae5af9c36a34
Sumitha7 <42170036+Sumitha7@users.noreply.github.com>
2018-08-09 12:46:19 +0530

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,19 @@
+class task1
+{
+  public static void main(String args[])
+  {
+  int a=3;
+  if(a<0)
+  {
+  System.out.println("the number is negative");
+  }
+  else if(a>0)
+  {
+  System.out.println("the number is positive");
+  }
+  else
+  {
+  System.out.println("the number is zero");
+  }
+ }
+ }
